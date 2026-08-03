eb7a08a439aa7056440ab83133467b796ef1dd43
Maria Clara Liossi <mariaclaraliossi@gmail.com>
2026-08-03 19:31:21 -0300

    Subindo o repositório

---
+++ src/main/java/br/com/allthings/allthings/AllthingsApplication.java
@@ -0,0 +1,13 @@
+package br.com.allthings.allthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllthingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllthingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allthings/allthings/AllthingsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allthings.allthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllthingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
